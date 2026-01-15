f285b542224574207da403786e2b5735ef96332d
hibaNITT <hibafathima.s2025@gmail.com>
2026-01-15 14:46:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+//alert("hi");
+
+//creating sounds via clicks
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i =0 ; i < numberOfButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var letter = this.innerHTML;
+        makeSound(letter);
+        buttonAnimation(letter);
+       
+    })
+}
+
+//creating sounds via key presses
+
+document.addEventListener("keydown",
+    function(event){
+        makeSound(event.key);
+                buttonAnimation(event.key);
+
+    })
+
+    //key detection function
+function makeSound(key){
+     switch(key){
+            case "w":
+                var crash = new Audio("/sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kickbass= new Audio("/sounds/kick-bass.mp3");
+                kickbass.play();
+                break;
+             case "s":
+                var snare = new Audio("/sounds/snare.mp3");
+                snare.play();
+                break;               
+            case "d":
+                var tom1 = new Audio("/sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio("/sounds/tom-2.mp3");
+                tom2.play();
+                break;  
+            case "k":
+                var tom3 = new Audio("/sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "l":
+                var tom4 = new Audio("/sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            default: 
+            alert("error");
+        }
+}
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100)
+
+}
